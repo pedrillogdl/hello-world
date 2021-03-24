f06daceb7a302770449d850af01eb04faca1cc7f
Pedro <jovenmzt20@hotmail.com>
2021-03-24 00:04:38 -0600

    Updating code for matching standards pycodestyle

--- Lab6.1/convert2X.py
+++ Lab6.1/convert2X.py
@@ -1,62 +1,63 @@
 
 def convertBinary(decimalNumber):
-	binaryNumber=[]
-	rightBinaryNumber=[]
-	leftover=decimalNumber
-	while leftover!=1:
-		binaryNumber.append(leftover%2)
-		leftover=leftover//2
-	binaryNumber.append(leftover)
-	for i in binaryNumber:
-		rightBinaryNumber.insert(0,i)
-	return rightBinaryNumber
+    binaryNumber = []
+    rightBinaryNumber = []
+    leftover = decimalNumber
+    while leftover != 1:
+        binaryNumber.append(leftover % 2)
+        leftover = leftover // 2
+    binaryNumber.append(leftover)
+    for i in binaryNumber:
+        rightBinaryNumber.insert(0, i)
+    return rightBinaryNumber
+
 
 def convertHexadecimal(decimalNumber):
-	hexaNumber=[]
-	rightHexaNumber=[]
-	leftoverNumber=decimalNumber
-	while leftoverNumber>15:
-		hexaNumber.append(leftoverNumber%16)
-		leftoverNumber=leftoverNumber//16
-	hexaNumber.append(leftoverNumber)
-	for i in hexaNumber:
-		if i==10:
-			i="A"
-		if i==11:
-			i="B"
-		if i==12:
-			i="C"
-		if i==13:
-			i="D"
-		if i==14:
-			i="E"
-		if i==15:
-			i="F"
-		rightHexaNumber.insert(0,i)
-	return rightHexaNumber
+    hexaNumber = []
+    rightHexaNumber = []
+    leftoverNumber = decimalNumber
+    while leftoverNumber > 15:
+        hexaNumber.append(leftoverNumber % 16)
+        leftoverNumber = leftoverNumber // 16
+    hexaNumber.append(leftoverNumber)
+    for i in hexaNumber:
+        if i == 10:
+            i = "A"
+        if i == 11:
+            i = "B"
+        if i == 12:
+            i = "C"
+        if i == 13:
+            i = "D"
+        if i == 14:
+            i = "E"
+        if i == 15:
+            i = "F"
+        rightHexaNumber.insert(0, i)
+    return rightHexaNumber
 
 
-print ("Program for converting a decimal number into a binary and hexadecimal number")
+print("Converting a decimal number into a binary and hexadecimal number")
 
 while True:
-	try:
-		numberToConvert= int(input("Please Enter the number to convert: "))
-		assert numberToConvert >= 0
-	except ValueError:
-		print("Oops!  That was no valid number.  Try again...")
-	except AssertionError :
-		print("Please enter a number bigger or equal to 0")
-	else:
-		break
+    try:
+        numberToConvert = int(input("Please Enter the number to convert: "))
+        assert numberToConvert >= 0
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+    except AssertionError:
+        print("Please enter a number bigger or equal to 0")
+    else:
+        break
 
-if numberToConvert==0:
-	finalBinaryNumber=0
-	finalHexaNumber=0
+if numberToConvert == 0:
+    finalBinaryNumber = 0
+    finalHexaNumber = 0
 else:
-	result=convertBinary(numberToConvert)
-	resultHexa=convertHexadecimal(numberToConvert)
-	finalBinaryNumber = ' '.join([str(elem) for elem in result]) 
-	finalHexaNumber = ' '.join([str(elem) for elem in resultHexa])
+    result = convertBinary(numberToConvert)
+    resultHexa = convertHexadecimal(numberToConvert)
+    finalBinaryNumber = ' '.join([str(elem) for elem in result])
+    finalHexaNumber = ' '.join([str(elem) for elem in resultHexa])
 
 print("The number " + str(numberToConvert) + " converted to binary is: " + str(finalBinaryNumber))
 print("The number " + str(numberToConvert) + " converted to hexadecimal is: " + str(finalHexaNumber))
--- Lab6.1/findNumber.py
+++ Lab6.1/findNumber.py
@@ -2,61 +2,49 @@ import random
 
 
 def guessNumberOp(customNumber):
-    
-    
-    if customNumber<surpriseNumber:
-        message="The Number is too low"
-    if customNumber>surpriseNumber:
-        message="The Number is too High"
-    if customNumber==surpriseNumber:
-        message="The Number is exactly right!"
+    if customNumber < surpriseNumber:
+        message = "The Number is too low"
+    if customNumber > surpriseNumber:
+        message = "The Number is too High"
+    if customNumber == surpriseNumber:
+        message = "The Number is exactly right!"
     return message
 
 
-
-print ("We are going to guess a number between 1 and 30, Please try to find the number")
-
-surpriseNumber=random.randint(1, 30)
+print("We are going to guess a number between 1 and 30, Please try to find it")
+surpriseNumber = random.randint(1, 30)
 
 
-guessNumber=input("Please Enter the number to guess or exit for ending the game: ")
-earlyExit="FALSE"
-if guessNumber!="exit":
-	guessingSteps=[]
-	guessingSteps.append(guessNumber)
-	earlyExit="TRUE"
-while  guessNumber!="exit":
-	result=guessNumberOp(int(guessNumber))
-	print(result)
-	if result=="The Number is exactly right!":
-		break
-	else:
-		guessNumber=input("Please Enter the number to guess or exit for ending the game: ")
-		if guessNumber!="exit":
-			guessingSteps.append(guessNumber)
+guessNumber = input("Please Enter the number to guess or exit for ending the game: ")
+earlyExit = "FALSE"
+if guessNumber != "exit":
+    guessingSteps = []
+    guessingSteps.append(guessNumber)
+    earlyExit = "TRUE"
+while guessNumber != "exit":
+    result = guessNumberOp(int(guessNumber))
+    print(result)
+    if result == "The Number is exactly right!":
+        break
+    else:
+        guessNumber = input("Please Enter the number to guess or exit for ending the game: ")
+        if guessNumber != "exit":
+            guessingSteps.append(guessNumber)
 
-print ("This is the list of guessing steps that you tried: ")
-newFile= open("GuessingSteps.txt","w+")
+print("This is the list of guessing steps that you tried: ")
+newFile = open("GuessingSteps.txt", "w+")
 newFile.write("This is the list of guessing steps that you tried: \r\n")
-if earlyExit=="TRUE":
-	for i in  guessingSteps:
-		print(i)
-		newFile.write("You guessed: " + str(i)+ "\r\n")
-	if guessNumber=="exit":
-		print("The number you were looking for was: " + str(surpriseNumber))
-		newFile.write("The number you were looking for was: " + str(surpriseNumber))
+if earlyExit == "TRUE":
+    for i in guessingSteps:
+        print(i)
+        newFile.write("You guessed: " + str(i) + "\r\n")
+    if guessNumber == "exit":
+        print("The number you were looking for was: " + str(surpriseNumber))
+        newFile.write("Number you were looking for was:" + str(surpriseNumber))
 else:
-	print("It is too sad that you didn´t want to play")
-	print("The number ready for being discovered was: " + str(surpriseNumber))
-	newFile.write("It is too sad that you didn´t want to play" + "\r\n")
-	newFile.write("The number you were looking for was: " + str(surpriseNumber))
+    print("It is too sad that you didn´t want to play")
+    print("The number ready for being discovered was: " + str(surpriseNumber))
+    newFile.write("It is too sad that you didn´t want to play" + "\r\n")
+    newFile.write("Number you were looking for was: " + str(surpriseNumber))
 
 newFile.close()
-
-
-
-
-
-
-
-
