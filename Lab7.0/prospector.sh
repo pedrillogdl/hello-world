10f053b4ddff024362b18769bef514d0e034047d
Pedro <jovenmzt20@hotmail.com>
2021-04-14 00:02:52 -0500

    Fixing prospector.sh

--- Lab7.0/prospector.sh
+++ Lab7.0/prospector.sh
@@ -1,13 +1,13 @@
 
 
-#PASSWORDUSER=$1
-#if [ -d hello-world ]; then
-#	rm -rf hello-world
-#fi
+PASSWORDUSER=$1
+if [ -d hello-world ]; then
+	rm -rf hello-world
+fi
 
 #git clone https://pedrillogdl:$PASSWORDUSER@github.com/pedrillogdl/hello-world.git
 
-cd hello-world/Lab6.1
+cd hello-world/Lab7.0
 prospector > ReportProspector.txt
 echo "This is the report for executing prospector(same content can be found in ReportProspector.txt)	:"
 cat ReportProspector.txt
