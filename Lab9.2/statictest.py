1d95155a47ea27301784031e82277e40a87f27fc
Pedro <jovenmzt20@hotmail.com>
2021-05-10 01:13:32 -0500

    Creating statictest.py

---
+++ Lab9.2/statictest.py
@@ -0,0 +1,16 @@
+import unittest
+import pycodestyle
+
+
+class TestCodeFormat(unittest.TestCase):
+
+    def test_conformance(self):
+        """Test that we conform to PEP-8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['class_unitTest.py', 'filecmp_unitTest.py', 'mathCeil_unitTest.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+if __name__ == '__main__':
+    unittest.main()
